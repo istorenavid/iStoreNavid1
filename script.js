618732e9669a88fa218b585bd2acef76d72826b3
istorenavid <istore.navid@gmail.com>
2026-07-21 00:12:16 +0330

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const modal=document.getElementById('guideModal');
-document.getElementById('guideBtn').addEventListener('click',()=>{modal.classList.add('show');modal.setAttribute('aria-hidden','false')});
-document.getElementById('closeModal').addEventListener('click',()=>{modal.classList.remove('show');modal.setAttribute('aria-hidden','true')});
-modal.addEventListener('click',e=>{if(e.target===modal){modal.classList.remove('show');modal.setAttribute('aria-hidden','true')}});
